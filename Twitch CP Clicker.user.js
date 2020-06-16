49a56347dda906612016b9acba41f51a16cf3294
Nydendan <30836018+Nydendan@users.noreply.github.com>
2020-06-16 16:26:58 +0200

    Update Twitch CP Clicker.user.js

--- Twitch CP Clicker.user.js
+++ Twitch CP Clicker.user.js
@@ -17,8 +17,8 @@
         if (elementExists){
             document.querySelector(selector).click();
             console.log("The button has been clicked.");
-        }
 	}
+    }
 
     setInterval(clickIT,5000);
 
