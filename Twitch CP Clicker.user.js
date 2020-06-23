045a0b66823a4a1462a063259ab459876d5762f2
Nydendan <30836018+Nydendan@users.noreply.github.com>
2020-06-24 01:04:43 +0200

    Update Twitch CP Clicker.user.js

--- Twitch CP Clicker.user.js
+++ Twitch CP Clicker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch CP Clicker
-// @version      0.1
+// @version      0.1.0
 // @description  Click +50 channel points button on twitch.tv automatically
 // @author       Nydendan
 // @match        https://www.twitch.tv/*
@@ -10,16 +10,20 @@
 (function() {
     'use strict';
 
-    const selector = "div > div.tw-block.tw-flex-grow-0.tw-flex-shrink-0.tw-full-height.tw-relative > div > div > div > div > section > div > div.chat-input.tw-block.tw-pd-b-1.tw-pd-x-1 > div:nth-child(2) > div.chat-input__buttons-container.tw-flex.tw-justify-content-between.tw-mg-t-1 > div:nth-child(1) > div > div > div > div.tw-full-height.tw-relative.tw-z-above > div > div > div > button";
-
-    function clickIT(){
-        var elementExists = document.querySelector(selector);
-        if (elementExists){
+    function clickIT() {
+        var button = document.querySelector("div.tw-full-height.tw-relative.tw-z-above > div > div > div > button")
+        if (button){
             document.querySelector(selector).click();
             console.log("The button has been clicked.");
         }
     }
-
-    setInterval(clickIT,5000);
-
+    
+    try {
+        setInterval(clickIT,5000);
+    }
+    
+    catch (error) {
+    console.error(error);
+    }
+    
 })();
